86e6320c6370be13f1357960978ceff0bf068242
Litiecheng <496498430@qq.com>
2021-03-10 22:04:46 +0800

    接入axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-// import axios from "axios";
+import axios from "axios";
+
+
 
 createApp(App).use(store).use(router).mount("#app");
+
+
+axios({
+    url:"http://localhost:9090/user/getUsers"
+}).then(res=>{
+    console.log(res);
+})
