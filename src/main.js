be41f23d9f25b2bd49eea3c5ec06bedc05366098
Litiecheng <496498430@qq.com>
2021-03-14 22:09:38 +0800

    修改axios

--- src/main.js
+++ src/main.js
@@ -4,13 +4,11 @@ import router from "./router";
 import store from "./store";
 import axios from "axios";
 
-
-
 createApp(App).use(store).use(router).mount("#app");
 
-
 axios({
-    url:"http://localhost:9090/user/getUsers"
+    url:"http://localhost:9090/user/getUsers",
+    method:"get"
 }).then(res=>{
     console.log(res);
 })
